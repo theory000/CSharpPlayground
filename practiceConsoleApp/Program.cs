0767d6cb9d7d80d3d689b4bff2a2b066fdf07f7e
Salman Abid <theory252@gmail.com>
2024-01-29 21:28:57 +0500

    Taking Age

--- practiceConsoleApp/Program.cs
+++ practiceConsoleApp/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter your age: ");
+string nameInput = Console.ReadLine();
+
+int age = 0; // Initialize age as 0 in case of conversion failure
+
+if (Int32.TryParse(nameInput, out age))
+{
+    Console.WriteLine("Your age is: " + age);
+    Console.WriteLine("The data type of age is now: " + age.GetType());
+}
+else
+{
+    Console.WriteLine("Invalid input. Please enter a valid integer for your age.");
+}
