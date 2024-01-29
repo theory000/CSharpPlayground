868a906b775191bfab79dd14314f5d321569a55d
Salman Abid <theory252@gmail.com>
2024-01-29 22:55:27 +0500

    Calculate Marks

--- practiceConsoleApp/Program.cs
+++ practiceConsoleApp/Program.cs
@@ -1,4 +1,26 @@
-﻿Console.Write("write your age: ");
-int age = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Your age is: " + age);
-Console.WriteLine(age.GetType());
+﻿using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Marks Calculator
+      Console.Write("Enter your maths marks: ");
+      int mats = Convert.ToInt32(Console.ReadLine());
+
+      Console.Write("Enter your science marks: ");
+      int sci = Convert.ToInt32(Console.ReadLine());
+
+      Console.Write("Enter your social studies marks: ");
+      int sos = Convert.ToInt32(Console.ReadLine());
+
+      Console.Write("Enter your geography marks: ");
+      int geo = Convert.ToInt32(Console.ReadLine());
+
+      int totalMarks = Convert.ToInt32(mats + sci + sos + geo);
+      Console.WriteLine("Your total Marks are: " + totalMarks);
+     }
+  }
+}
