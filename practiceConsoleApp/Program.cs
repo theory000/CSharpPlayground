9ef36640427cc38efe80634d1293b654f1f24337
Salman Abid <theory252@gmail.com>
2024-02-04 17:52:55 +0500

    updated file

--- practiceConsoleApp/Program.cs
+++ practiceConsoleApp/Program.cs
@@ -1,33 +1,15 @@
 ﻿using System;
 
-namespace RandomNumberGenerator
+namespace String
 {
   class Program
   {
     static void Main(string[] args)
     {
 
-      int min, max;
-
-      Console.WriteLine("Enter Minimum Value: ");
-      min = Convert.ToInt32(Console.ReadLine());
-
-      Console.WriteLine("Enter Maximum Value: ");
-      max = Convert.ToInt32(Console.ReadLine());
-
-      Random random = new Random();
-
-      // Generate a random double between 0 and 1
-      double randomDouble = random.NextDouble();
-
-      // Calculate the range considering negative values are possible
-      int range = (max - min) + 1;
-
-      // Multiply the random double by the range and cast to int
-      int randomNum = (int)(randomDouble * range) + min;
-
-      Console.WriteLine("Random number: {0}", randomNum);
-
+      Console.WriteLine("I'm \nViking");
+      Console.WriteLine("I'm \tViking");
+      Console.WriteLine("I'm \bViking");
 
     }
   }
